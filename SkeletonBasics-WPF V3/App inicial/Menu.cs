4ea1d9c063fa7427806203ac3cfbd005c17127ba
Ariel Molina <arielhmolina@gmail.com>
2018-10-06 13:55:52 -0300

    http rest

--- SkeletonBasics-WPF V3/App inicial/Menu.cs
+++ SkeletonBasics-WPF V3/App inicial/Menu.cs
@@ -6,6 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 
 namespace Microsoft.Samples.Kinect.SkeletonBasics.App_inicial
 {
@@ -28,7 +33,31 @@ namespace Microsoft.Samples.Kinect.SkeletonBasics.App_inicial
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string url = "https://ataxia-services-project.herokuapp.com/token";
+            var request = (HttpWebRequest)WebRequest.Create(url);
 
+            request.Method = "GET";
+            //request.UserAgent = RequestConstants.UserAgentValue;
+            //request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var content = string.Empty;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+            var rta = JsonConvert.DeserializeObject<dynamic>(content);
+            Console.WriteLine(rta.body.token);
+            //eturn content;
+        
         }
     }
 }
+
