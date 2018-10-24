281913c07ee874df5cf561c3372b351eba252ef7
Ariel Molina <arielhmolina@gmail.com>
2018-10-24 16:26:00 -0300

    UI

--- AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Models/InicioViewModel.cs
+++ AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Models/InicioViewModel.cs
@@ -9,7 +9,10 @@ namespace AtaxiaVision.Models
     class InicioViewModel
     {
         public string Token { get; set; }
+        // Indica si el token es valido o invalido, una vez que se verifico con el servidor.
         public bool TokenValido { get; set; }
         public int Ejercicio { get; set; }
+        // Indica si el token fue verificado contra el servidor.
+        public bool TokenVerificado { get; set; }
     }
 }
