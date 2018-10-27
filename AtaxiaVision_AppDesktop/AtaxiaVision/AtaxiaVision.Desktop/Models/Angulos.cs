2d4ecc64d626b79742a0ca8372e5af72f964d971
camicasuscelli <caami.casuscelli@gmail.com>
2018-10-27 18:54:25 -0300

    creacion de clase modelo Angulos

--- AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Models/Angulos.cs
+++ AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Models/Angulos.cs
@@ -6,35 +6,21 @@ using System.Threading.Tasks;
 
 namespace AtaxiaVision.Models
 {
-    public class TensionServos
+    public class Angulos
     {
         public double CodoArribaAbajo { get; set; }
         public double CodoIzquierdaDerecha { get; set; }
         public double HombroArribaAbajo { get; set; }
         public double HombroAdelanteAtras { get; set; }
 
-        public TensionServos(string datoArduino)
+        public Angulos()
         {
-            //el string llega en el formato:
-            //A: codoArribaAbajo
-            //B: codoIzquierdaDerecha
-            //C: hombroArribaAbajo
-            //D: hombroAdelanteAtras
-
-            //primero llega un *
-            if (datoArduino[0].Equals("*"))
-            {
-                //obtengo el index de las letras:
-                int indexA = datoArduino.IndexOf("A");
-                int indexB = datoArduino.IndexOf("B");
-                int indexC = datoArduino.IndexOf("C");
-                int indexD = datoArduino.IndexOf("D");
-
-                CodoArribaAbajo = double.Parse(datoArduino.Substring(indexA, indexB));
-                CodoIzquierdaDerecha = double.Parse(datoArduino.Substring(indexB, indexC));
-                HombroArribaAbajo = double.Parse(datoArduino.Substring(indexC, indexD));
-                HombroAdelanteAtras = double.Parse(datoArduino.Substring(indexD));
-            }
+            CodoArribaAbajo = -1;
+            CodoIzquierdaDerecha = -1;
+            HombroArribaAbajo = -1;
+            HombroAdelanteAtras = -1;
         }
+
+
     }
 }
