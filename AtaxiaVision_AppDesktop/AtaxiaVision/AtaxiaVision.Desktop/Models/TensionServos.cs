7f40244c36ee7ab6d6c774135624e39876b9ac87
camicasuscelli <caami.casuscelli@gmail.com>
2018-10-27 18:54:07 -0300

    update Tension Servos

---
+++ AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Models/TensionServos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtaxiaVision.Models
+{
+    public class TensionServos
+    {
+        public double CodoArribaAbajo { get; set; }
+        public double CodoIzquierdaDerecha { get; set; }
+        public double HombroArribaAbajo { get; set; }
+        public double HombroAdelanteAtras { get; set; }
+
+        public TensionServos(string datoArduino)
+        {
+            //el string llega en el formato:
+            //A: codoArribaAbajo
+            //B: codoIzquierdaDerecha
+            //C: hombroArribaAbajo
+            //D: hombroAdelanteAtras
+
+            //primero llega un *
+            if (datoArduino[0].Equals("*"))
+            {
+                //obtengo el index de las letras:
+                int indexA = datoArduino.IndexOf("A");
+                int indexB = datoArduino.IndexOf("B");
+                int indexC = datoArduino.IndexOf("C");
+                int indexD = datoArduino.IndexOf("D");
+
+                CodoArribaAbajo = double.Parse(datoArduino.Substring(indexA, indexB));
+                CodoIzquierdaDerecha = double.Parse(datoArduino.Substring(indexB, indexC));
+                HombroArribaAbajo = double.Parse(datoArduino.Substring(indexC, indexD));
+                HombroAdelanteAtras = double.Parse(datoArduino.Substring(indexD));
+            }
+        }
+    }
+}
