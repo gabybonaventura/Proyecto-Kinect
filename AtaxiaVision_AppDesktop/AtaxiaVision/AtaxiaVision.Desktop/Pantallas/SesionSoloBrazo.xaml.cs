5c7efabf0b6ec2b8a27f626061129ee621593814
Ariel <Ariel@ARIEL-DESKTOP>
2018-11-08 20:03:40 -0300

    Ejercicio manual

--- AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Pantallas/SesionSoloBrazo.xaml.cs
+++ AtaxiaVision_AppDesktop/AtaxiaVision/AtaxiaVision.Desktop/Pantallas/SesionSoloBrazo.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using AtaxiaVision.Controllers;
 using AtaxiaVision.Desktop.Pantallas;
+using AtaxiaVision.Helpers;
 using AtaxiaVision.Models;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -40,14 +42,111 @@ namespace AtaxiaVision.Pantallas
         public RespuestaToken Token { get; set; }
         public bool RepeticionAutomatica { get; set; }
         public int DelaySeg { get; set; }
+        public int Repeticiones { get; set; }
         ArduinoController arduinoController = new ArduinoController();
 
         public delegate void EstadoLabelDelegate(string nombre);
         public EstadoLabelDelegate estadoLabelDelegate;
         public delegate void RepetecionesLabelDelegate(int r);
         public RepetecionesLabelDelegate repetecionesLabelDelegate;
+        public delegate void RepeticionButtonDelegate(bool isEnable);
+        public RepeticionButtonDelegate repeticionButtonDelegate;
 
         private BackgroundWorker ejercicioAutomaticoBG = new BackgroundWorker();
+        private BackgroundWorker ejercicioManualBG = new BackgroundWorker();
+
+        #region Delegates
+        public delegate void SnackBarDelegate(string msg);
+        public SnackBarDelegate snackBarDelegate;
+        public delegate void ConsumoHombroArribaAbajoDelegate(int consumo);
+        public ConsumoHombroArribaAbajoDelegate consumoHombroArribaAbajoDelegate;
+        public delegate void ConsumoHombroAdelanteAtras(int consumo);
+        public ConsumoHombroAdelanteAtras consumoHombroAdelanteAtrasDelegate;
+        public delegate void ConsumoCodoArribaAbajo(int consumo);
+        public ConsumoCodoArribaAbajo consumoCodoArribaAbajoDelegate;
+        public delegate void ConsumoCodoDerechaIzquierda(int consumo);
+        public ConsumoCodoDerechaIzquierda consumoCodoDerechaIzquierdaDelegate;
+
+        private BackgroundWorker TensionesServosBackgroundWorker = new BackgroundWorker();
+        private BackgroundWorker ArduinoActivoBackgroundWorker = new BackgroundWorker();
+        #endregion
+
+        #region Metodos Delegates
+        private void EstadoSnackBar(string mensaje)
+        {
+            Snackbar.IsActive = false;
+            Snackbar.Message = new SnackbarMessage() { Content = mensaje };
+            Snackbar.IsActive = true;
+        }
+        private void SetConsumoHombroArribaAbajo(int consumo)
+        {
+            ConsumoHombroArribaAbajoLabel.Foreground = ArduinoHelper.CalcularColor(consumo);
+            ConsumoHombroArribaAbajoLabel.Content = consumo + " mA";
+        }
+        private void SetConsumoHombroAdelanteAtras(int consumo)
+        {
+            ConsumoHombroAdelanteAtrasLabel.Foreground = ArduinoHelper.CalcularColor(consumo);
+            ConsumoHombroAdelanteAtrasLabel.Content = consumo + " mA";
+        }
+        private void SetConsumoCodoArribaAbajo(int consumo)
+        {
+            ConsumoCodoArribaAbajoLabel.Foreground = ArduinoHelper.CalcularColor(consumo);
+            ConsumoCodoArribaAbajoLabel.Content = consumo + " mA";
+        }
+        private void SetConsumoCodoDerechaIzquierda(int consumo)
+        {
+            ConsumoCodoDerechaIzquierdaLabel.Foreground = ArduinoHelper.CalcularColor(consumo);
+            ConsumoCodoDerechaIzquierdaLabel.Content = consumo + " mA";
+        }
+        private void MostrarConsumosDelegates()
+        {
+            TensionServos tensiones;
+            if (arduinoController == null || arduinoController.UltimaTension == null)
+            {
+                tensiones = new TensionServos()
+                {
+                    CodoArribaAbajo = 20,
+                    CodoIzquierdaDerecha = 105,
+                    HombroAdelanteAtras = 215,
+                    HombroArribaAbajo = 545
+                };
+            }
+            else
+                tensiones = arduinoController.UltimaTension;
+
+            ConsumoHombroArribaAbajoLabel.Dispatcher.Invoke(consumoHombroArribaAbajoDelegate, tensiones.HombroArribaAbajo);
+            ConsumoHombroAdelanteAtrasLabel.Dispatcher.Invoke(consumoHombroAdelanteAtrasDelegate, tensiones.HombroAdelanteAtras);
+            ConsumoCodoArribaAbajoLabel.Dispatcher.Invoke(consumoCodoArribaAbajoDelegate, tensiones.CodoArribaAbajo);
+            ConsumoCodoDerechaIzquierdaLabel.Dispatcher.Invoke(consumoCodoDerechaIzquierdaDelegate, tensiones.CodoIzquierdaDerecha);
+        }
+        private void TensionesServosBG()
+        {
+            var bg = TensionesServosBackgroundWorker;
+            bg.DoWork += (s, e) =>
+            {
+                while (true)
+                {
+                    MostrarConsumosDelegates();
+                    Thread.Sleep(1000);
+                }
+            };
+
+            if (!bg.IsBusy)
+                bg.RunWorkerAsync();
+        }
+        private void ArduinoActivoBG()
+        {
+            var bg = ArduinoActivoBackgroundWorker;
+            bg.DoWork += (s, e) =>
+            {
+                Thread.Sleep(1000);
+                if (arduinoController == null || arduinoController.UltimaTension == null)
+                    Snackbar.Dispatcher.Invoke(snackBarDelegate, "Exoesqueleto no detectado.");
+            };
+            if (!bg.IsBusy)
+                bg.RunWorkerAsync();
+        }
+        #endregion
 
         public SesionSoloBrazo(RespuestaToken token = null)
         {
@@ -78,11 +177,24 @@ namespace AtaxiaVision.Pantallas
             RepeticionesLabel.Content = r +"/" + Token.Repeticiones;
         }
 
+        private void EstadoRepetecionesButton(bool isEnable)
+        {
+            RepeticionButton.IsEnabled = isEnable;
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             estadoLabelDelegate = new EstadoLabelDelegate(EstadoCard);
             repetecionesLabelDelegate = new RepetecionesLabelDelegate(MostrarRepeticiones);
-
+            repeticionButtonDelegate = new RepeticionButtonDelegate(EstadoRepetecionesButton);
+            snackBarDelegate = new SnackBarDelegate(EstadoSnackBar);
+            consumoHombroArribaAbajoDelegate = new ConsumoHombroArribaAbajoDelegate(SetConsumoHombroArribaAbajo);
+            consumoHombroAdelanteAtrasDelegate = new ConsumoHombroAdelanteAtras(SetConsumoHombroAdelanteAtras);
+            consumoCodoArribaAbajoDelegate = new ConsumoCodoArribaAbajo(SetConsumoCodoArribaAbajo);
+            consumoCodoDerechaIzquierdaDelegate = new ConsumoCodoDerechaIzquierda(SetConsumoCodoDerechaIzquierda);
+            TensionesServosBG();
+            ArduinoActivoBG();
+            EjecutarEjercicioManualBG();   // Preparo ejercicio manual
 
             LlenarComboBoxs();          // Lleno los combo boxs
             MostrarRepeticiones(0);      // Lleno el campo de repeticiones
@@ -141,7 +253,7 @@ namespace AtaxiaVision.Pantallas
 
         private void RepeticionButton_Click(object sender, RoutedEventArgs e)
         {
-
+            EjecutarEjercicioManual();
         }
 
         public void EstadoCard(string estado)
@@ -172,9 +284,33 @@ namespace AtaxiaVision.Pantallas
                 ejercicioAutomaticoBG.RunWorkerAsync();
         }
 
-        public void EjecutarEjercicioManual()
+        private void EjecutarEjercicioManual()
         {
+            if (!ejercicioManualBG.IsBusy)
+                ejercicioManualBG.RunWorkerAsync();
+        }
 
+        public void EjecutarEjercicioManualBG()
+        {
+            ejercicioManualBG.DoWork += (s, e) =>
+            {
+                RepeticionButton.Dispatcher.Invoke(repeticionButtonDelegate, false);
+                RepeticionesLabel.Dispatcher.Invoke(repetecionesLabelDelegate, (Repeticiones + 1));
+                EstadoLabel.Dispatcher.Invoke(estadoLabelDelegate, "Inicio repetición: " + (Repeticiones + 1));
+                Thread.Sleep(2 * 1000);
+                arduinoController.EnviarAngulosFromAngulosServos(new AngulosServos(Token.Ejercicio.EstadoInicial));
+                EstadoLabel.Dispatcher.Invoke(estadoLabelDelegate, "Posicionando Estado Inicial [" + Token.Ejercicio.EstadoInicial + "]");
+                Thread.Sleep(DelaySeg * 1000);
+                EstadoLabel.Dispatcher.Invoke(estadoLabelDelegate, "Posicionando Estado Final [" + Token.Ejercicio.EstadoFinal + "]");
+                arduinoController.EnviarAngulosFromAngulosServos(new AngulosServos(Token.Ejercicio.EstadoFinal));
+                Thread.Sleep(DelaySeg * 1000);
+                Repeticiones++;
+                EstadoLabel.Dispatcher.Invoke(estadoLabelDelegate, "Fin Repetición");
+                if (Repeticiones < Token.Repeticiones)
+                    RepeticionButton.Dispatcher.Invoke(repeticionButtonDelegate, true);
+                else
+                    RepeticionButton.Dispatcher.Invoke(repeticionButtonDelegate, false);
+            };
         }
     }
 }
